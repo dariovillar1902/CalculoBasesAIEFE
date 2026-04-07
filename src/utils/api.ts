937bedd236c6ffdbbdd5dde82d66befa0ce920d9
Darío Javier Villar <dario_villar2001@hotmail.com>
2026-04-07 03:32:33 -0300

    fix: update API base URL to Render deployment

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://calculobasesaie-production.up.railway.app/api/",
+  baseURL: "https://calculobasesaie.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default api;
+
